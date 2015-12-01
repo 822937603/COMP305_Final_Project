fd11b1a3118feec3f29b5426797a1b85d2be41c2
Jonathan Lee <jonlee50@hotmail.com>
2015-11-30 20:47:06 -0500

    Commit 17
    Items Placed, Pickup Broken

---
+++ An Ancient Score/Assets/_Scripts/GoldItemController.cs
@@ -0,0 +1,43 @@
+﻿// Jonathan Lee
+// File: GoldItemController.cs
+// Controls the collection of gold item on the first level
+// Sound, collision, and scoring
+
+using UnityEngine;
+using System.Collections;
+
+public class GoldItemController : MonoBehaviour {
+
+
+	// PUBLIC INSTANCE VARIABLES
+
+	public AudioSource goldCollect;
+	public GameObject gold;
+
+	// Use this for initialization
+	void Start () {
+
+		goldCollect = GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void DestroyGold ()
+	{
+		Destroy (gameObject);
+	}
+
+	void onTriggerEnter(Collider otherGameObject)
+	{
+		if (otherGameObject.tag == "Player") {
+			Debug.Log("col");
+			goldCollect.Play();
+			Invoke("DestroyGold", 0.5f);
+		}
+	}
+}
+
