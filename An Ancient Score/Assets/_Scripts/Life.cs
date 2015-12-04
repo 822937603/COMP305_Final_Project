261f2535bd2cd6de64d971255485cffbd16e8b1a
InstantChaos <fnbeddie@hotmail.com>
2015-12-03 21:57:38 -0500

    Commit 20

---
+++ An Ancient Score/Assets/_Scripts/Life.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour {
+
+	public Text lifeLabel;
+	public int lifecount = 5;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		//calls the setLife method to set the life counter
+		this.setLife ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//set the number of life available for the player.
+	private void setLife(){
+		this.lifeLabel.text = "Life: " + this.lifecount;
+	}
+	
+	//Life will be deducted upon sensing a collision.
+	void OnTriggerEnter(Collider otherObjects){
+		if (otherObjects.tag == "Enemy") {
+			this.lifecount -= 1;
+		}
+		//Restarts the level when the player loses all their life.
+		if(this.lifecount <=0){
+			Application.LoadLevel(Application.loadedLevel);
+			
+		}
+		this.setLife ();		
+	}
+}
