21290d9f2c106e5ae87219d6c124db04b67c86eb
InstantChaos <fnbeddie@hotmail.com>
2015-12-07 15:39:57 -0500

    fixed the colliders

---
+++ An Ancient Score/Assets/_Scripts/Score.cs
@@ -0,0 +1,46 @@
+﻿// Franco Chong
+// File: Life.cs
+// Controls the score for the player
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreLabel;
+	public int Scorecount = 0;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		//calls the setScore method to set the score counter
+		this.setScore ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//set the score for the player
+	private void setScore(){
+		this.scoreLabel.text = "Score: " + this.Scorecount;
+	}
+	
+	//Score will be added
+	void OnTriggerEnter(Collider otherObjects){
+		if (otherObjects.CompareTag("Gold")) {
+			this.Scorecount += 50;
+		}
+		if (otherObjects.CompareTag ("ArrowShaft")) {
+			this.Scorecount +=50;
+		}
+		if (otherObjects.CompareTag ("Wood")) {
+			this.Scorecount +=50;
+		}
+		this.setScore ();		
+	}
+}
