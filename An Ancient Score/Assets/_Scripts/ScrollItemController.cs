c3e8c16a775b68b232b70390d6bfda88ef8dc218
Jonathan Lee <jonlee50@hotmail.com>
2015-12-18 17:27:02 -0500

    FREAKING COLLIDERS
    fixed using gold script
    gg continuity

---
+++ An Ancient Score/Assets/_Scripts/ScrollItemController.cs
@@ -0,0 +1,34 @@
+﻿// Jonathan Lee
+// File: ScrollItemController.cs
+// Controls the collection of scroll item on the second level
+// Sound, collision, and scoring
+
+using UnityEngine;
+using System.Collections;
+
+public class ScrollItemController : MonoBehaviour {
+
+	//PUBLIC INSTANCE VARIABLES
+	public AudioSource scrollCollect;
+
+
+	// Use this for initialization
+	void Start () {
+		scrollCollect = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			scrollCollect.Play();
+			Destroy(gameObject,0.5f);
+		}
+	}
+}
+
+
